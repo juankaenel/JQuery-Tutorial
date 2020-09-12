20badbe318b2596535309300975cb827651161aa
Juan Kaenel <kaenel10@gmail.com>
2020-09-12 00:38:42 -0300

    primer commit

---
+++ 1-PrimerosPasos/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+//$('h1').html('Nuevo titulo'); // document.querySelector('h1').innerHTML = 'Nuevo titulo con js puro';
+//$('.titulo').html('Nuevo titulo desde clase'); //document.querySelector('.titulo').innerHTML = 'Nuevo titulo desde clase con js puro';
+//$('#idh1').html('Nuevo titulo desde id'); //document.querySelector('#idh1').innerHTML = 'Nuevo titulo desde id con js puro';
+
+//$('.container h1').html('cambio de nombres de todos los h1 de container');
+//$('.container h1:first').html('cambio de nombres de todos los h1 de container'); //solo accede al primer h1 del container
+//$('.container h1:last').html('cambio de nombres de todos los h1 de container'); //solo accede al primer h1 del container
+
+$('#idh1').addClass('text-danger');
+
+});
+
